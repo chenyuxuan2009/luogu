677459fbaa4fcd774949a12fbdeabae378d3970b
chenyuxuan2009 <chen20091119@foxmail.com>
2023-06-17 19:30:49 +0800

    卷王监视器支持绑定大小号

--- README.md
+++ README.md
@@ -8,8 +8,6 @@
 
 **卷王监视器使用原理是爬取指定用户前后提交于通过差，并没有实时访问 /record，没有违反 robots.txt。**
 
-进入 /monitor/monitorList.txt，输入你需要监视的用户的用户编号，**用逗号 `,` 隔开**。
+进入 /monitor/monitorList.txt，输入你需要监视的用户的用户编号，第一行放你对被监视用户的昵称，第二行开始是被监视用户的用户编号集合，用**逗号** `,` 隔开。具体可见本仓库的 /monitor/monitorList.txt。
 
 找到 monitor.py 运行。
-
-未来可能会更新支持大小号同步等功能。
--- monitor.py
+++ monitor.py
@@ -1,33 +1,48 @@
-import requests
-import json
-import time
 import os
+import sys
 from main import *
 
+def lineCount(file_name):
+    with open(file_name) as f:
+        for count, _ in enumerate(f, 1):
+            pass
+    return count
+
 file=open('monitor\\monitorList.txt','r',encoding='utf-8')
-monitorList=file.readline()
+lenn=lineCount('monitor\\monitorList.txt')
+if lenn&1:
+    print('文本行数不是偶数')
+    sys.exit()
+lenn>>=1
+monitorList={}
+for i in range(lenn):
+    name=file.readline()
+    name=name[0:len(name)-1]
+    monitorList[name]=list(map(int,file.readline().split(',')))
 file.close()
-monitorList=list(map(int,monitorList.split(',')))
-print(monitorList)
-for uid in monitorList:
-    data=getUser(uid)
-    user=data['currentData']['user']
-    if user['passedProblemCount'] != None:
-        Try=int(user['submittedProblemCount'])
-        AC=int(user['passedProblemCount'])
-    else:
-        print('用户编号为 '+str(uid)+' 的用户开启了完全隐私保护。')
-        continue
-    if os.path.exists(monitorListPath+'\\'+str(uid)+'.txt'):
-        # print('ok')
-        file=open(monitorListPath+'\\'+str(uid)+'.txt','r',encoding='utf-8')
-        ACL,TryL=map(int,file.readline().split(','))
-        file.close()
-        print('用户编号为 '+str(uid)+' 的用户距离上次运行监视器，又通过了 '+str(AC-ACL)+' 题，又提交了 '+str(Try-TryL)+' 发代码。')
-        saveData(str(AC)+','+str(Try),monitorListPath,str(uid)+'.txt')
-    else:
-        saveData(str(AC)+','+str(Try),monitorListPath,str(uid)+'.txt')
-        print('用户编号为 '+str(uid)+' 的用户第一次进入卷王监视名单。')
+
+for name in monitorList:
+    print('现在开始检查昵称为 '+name+' 的用户的做题情况。')
+    for uid in monitorList[name]:
+        time.sleep(0.1)
+        data=getUser(uid)
+        user=data['currentData']['user']
+        if user['passedProblemCount'] != None:
+            Try=int(user['submittedProblemCount'])
+            AC=int(user['passedProblemCount'])
+        else:
+            print('用户编号为 '+str(uid)+' 的用户开启了完全隐私保护。')
+            continue
+        if os.path.exists(monitorListPath+'\\'+str(uid)+'.txt'):
+            file=open(monitorListPath+'\\'+str(uid)+'.txt','r',encoding='utf-8')
+            ACL,TryL=map(int,file.readline().split(','))
+            file.close()
+            print('用户编号为 '+str(uid)+' 的用户距离上次运行监视器，又通过了 '+str(AC-ACL)+' 题，又提交了 '+str(Try-TryL)+' 发代码。')
+            saveData(str(AC)+','+str(Try),monitorListPath,str(uid)+'.txt')
+        else:
+            saveData(str(AC)+','+str(Try),monitorListPath,str(uid)+'.txt')
+            print('用户编号为 '+str(uid)+' 的用户第一次进入卷王监视名单。')
+    print('\n')
 # for uid in range(UidStart,UidEnd+1):
 #     data=getUser(uid)
     
